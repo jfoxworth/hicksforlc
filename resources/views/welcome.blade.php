aa33c25f90dec427148ef13eb709cc9c9af05d53
Joshua Foxworth <jfoxworth@cadwolf.com>
2020-09-17 00:36:17 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="SemiColonWeb" />
+
+    <!-- Stylesheets
+    ============================================= -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700|Roboto:300,400,500,700&display=swap" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+
+    <!-- One Page Module Specific Stylesheet -->
+    <link rel="stylesheet" href="css/onepage.css" type="text/css" />
+    <!-- / -->
+
+    <link rel="stylesheet" href="css/dark.css" type="text/css" />
+    <link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+    <link rel="stylesheet" href="css/et-line.css" type="text/css" />
+    <link rel="stylesheet" href="css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+
+    <link rel="stylesheet" href="css/fonts.css" type="text/css" />
+
+    <link rel="stylesheet" href="css/custom.css" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Document Title
+    ============================================= -->
+    <title>Justin Hicks for League City City Council</title>
+
+</head>
+
+<body class="stretched side-push-panel">
+
+    <div class="body-overlay"></div>
+
+
+    <!-- Document Wrapper
+    ============================================= -->
+    <div id="wrapper" class="clearfix">
+
+        <!-- Header
+        ============================================= -->
+        <header id="header" class="full-header transparent-header border-full-header dark header-size-custom" data-sticky-shrink="false" data-sticky-class="not-dark" data-sticky-offset="full" data-sticky-offset-negative="100">
+            <div id="header-wrap">
+                <div class="container">
+                    <div class="header-row">
+
+                        <!-- Logo
+                        ============================================= -->
+                        <div id="logo">
+                        </div><!-- #logo end -->
+
+
+                        <!-- Primary Navigation
+                        ============================================= -->
+                        <nav class="primary-menu">
+
+                            <ul class="one-page-menu menu-container" data-easing="easeInOutExpo" data-speed="1250" data-offset="65">
+                                <li class="menu-item">
+                                    <a href="#" class="menu-link" data-href="#wrapper"><div>Home</div></a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#" class="menu-link" data-href="#section-about"><div>About Me</div></a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#" class="menu-link" data-href="#section-works"><div>Issues</div></a>
+                                </li>
+                            </ul>
+
+                        </nav><!-- #primary-menu end -->
+
+                    </div>
+                </div>
+            </div>
+            <div class="header-wrap-clone"></div>
+        </header><!-- #header end -->
+
+        <!-- Slider
+        ============================================= -->
+        <section id="slider" class="slider-element slider-parallax dark min-vh-60 min-vh-md-100 include-header">
+            <div class="slider-inner" style="background-image: url('/images/watertower.jpg'); background-size: cover; background-position: center center;">
+
+                <div class="vertical-middle slider-element-fade">
+                    <div class="container dark text-center py-5">
+
+                        <div class="vertical-middle ignore-header">
+                            <div class="d-flex">
+                                <div class="col-lg-6 offset-lg-3">
+                                    <h2 class="display-4 text-uppercase ls2 mb-3" style="font-weight: 900;">Justin Hicks for League City Council</h2>
+                                    <p class="lead"></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </section><!-- #slider end -->
+
+        <section>
+            <div class="section dark m-0" style="background-color:#8B0000">
+                <div class="mx-auto center" style="max-width: 900px;">
+                    <h2 class="mb-0 font-weight-light ls1">Thank you for your support <a href="https://secure.anedot.com/justin-hicks-for-city-council/donate?fbclid=IwAR0c2Bak5IviSdOmboZfFDyteWTUbpNnLYDwbqIf_NMCH-4dFTql4WVcciQ" data-scrollto="#template-contactform" data-offset="140" data-easing="easeInOutExpo" data-speed="1250" class="button button-border button-circle button-light button-large my-0" style="position: relative; top: -3px;">Make a Donation</a></h2>
+                </div>
+            </div>
+        </section>
+
+        <!-- Content
+        ============================================= -->
+        <section id="content">
+            <div class="content-wrap py-0">
+
+                <div id="" class="center page-section">
+
+                    <div class="container clearfix">
+
+                    <div class="row col-mb-50">
+                        <div class="col-md-5">
+\                                <img src="images/headshot.jpg" alt="Image" class="w-100">
+                        </div>
+
+                        <div class="col-md-7">
+                            <div class="heading-block">
+                                <h2>Why I am running</h2>
+                            </div>
+
+                            <p>I am a Veteran, Conservative, Patriot, Father and Christian. I reject defeatism, and believe in fighting for what is right, and never give up. We need to ensure liberal policies stay out of our city. Let's preserve our core values, small business, support the police and first responders. Let's keep League City the same family friendly place I've grown to love.</p>
+
+                        </div>
+                    </div>
+
+
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </section><!-- #content end -->
+
+        <!-- Footer
+        ============================================= -->
+        <footer id="footer" class="dark border-0">
+
+            <!-- Copyrights
+            ============================================= -->
+            <div id="copyrights">
+                <div class="container">
+
+                    <div class="row col-mb-30">
+
+                        <div class="col-md-6 text-center text-md-left">
+                            PPA by Justin Hicks for League City Council
+                        </div>
+
+                        <div class="col-md-6 text-center text-md-right">
+                            <div class="d-flex justify-content-center justify-content-md-end">
+                                <a href="https://www.facebook.com/VoteJustinHicksLeagueCity" class="social-icon si-small si-borderless si-facebook">
+                                    <i class="icon-facebook"></i>
+                                    <i class="icon-facebook"></i>
+                                </a>
+                            </div>
+
+                            <div class="clear"></div>
+
+                            <i class="icon-envelope2"></i> Justin_Hicks@comcast.net <span class="middot">&middot;</span> <i class="icon-headphones"></i> 425-877-4025 <span class="middot">&middot;</span>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div><!-- #copyrights end -->
+
+
+        </footer><!-- #footer end -->
+
+    </div><!-- #wrapper end -->
+
+    <!-- Go To Top
+    ============================================= -->
+    <div id="gotoTop" class="icon-angle-up"></div>
+
+    <!-- JavaScripts
+    ============================================= -->
+    <script src="js/jquery.js"></script>
+    <script src="js/plugins.min.js"></script>
+
+    <!-- Footer Scripts
+    ============================================= -->
+    <script src="js/functions.js"></script>
+
+</body>
+</html>
